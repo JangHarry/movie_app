66e493a5e0878151f72dc9f23b046323cf3dee1d
incheonJJang <icjang123@gmail.com>
2020-11-08 20:53:37 +0900

    Updates

---
+++ precache-manifest.95175f2f61ef5ec122573c78b4f2d91f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9003c4ab4e4b1f89554833271dea8007",
+    "url": "/JangHarry/movie_app/index.html"
+  },
+  {
+    "revision": "8d8dc1a85b8faf819f9a",
+    "url": "/JangHarry/movie_app/static/css/main.c79ced86.chunk.css"
+  },
+  {
+    "revision": "cec983703ce11e77be72",
+    "url": "/JangHarry/movie_app/static/js/2.cace864e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/JangHarry/movie_app/static/js/2.cace864e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d8dc1a85b8faf819f9a",
+    "url": "/JangHarry/movie_app/static/js/main.6c719c7e.chunk.js"
+  },
+  {
+    "revision": "872107c2fa974e6b902c",
+    "url": "/JangHarry/movie_app/static/js/runtime-main.134b363b.js"
+  }
+]);
